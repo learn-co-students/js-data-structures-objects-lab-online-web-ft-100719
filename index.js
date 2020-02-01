32fa4bfde0ea84f74901e3f0c6df57be722c9486
Stefanie <stefcodes@gmail.com>
2020-02-01 15:29:39 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("The DOM has loaded");
+    updateDriverWithKeyAndValue()
+  })
+
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign(driver, { [key]: value });
+}
+
+ function deleteFromDriverByKey(driver, key) {
+//     // delete driver[key]
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}
+
